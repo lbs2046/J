bb3035aa2fb7c6fab61e832b25a955da3ef27126
Admin <lbs2046@yahoo.com>
2017-04-11 07:54:54 +0800

    2017JUL01 POC ready.

---
+++ HelloWorldApp.java
@@ -0,0 +1,17 @@
+package helloworldapp;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
